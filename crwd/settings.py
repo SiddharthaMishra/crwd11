b4e809b5faa62727f575ef84499df1eba47b2f10
Siddhartha Mishra <sidm1999@gmail.com>
2020-04-09 16:53:40 +0530

    fix

--- crwd/settings.py
+++ crwd/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'crwdapi',
     'rest_framework',
     'books',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
